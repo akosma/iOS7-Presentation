7f73ff439d36495101571b2a3901c71811efcdc9
Adrian Kosmaczewski <ako@trifork.com>
2013-11-14 14:52:03 +0100

    Added a longer time to the url session PDF snapshot, so that the screen shows meaningful values

--- Presentation/Classes/Controllers/Screens/TRIURLSessionScreen.m
+++ Presentation/Classes/Controllers/Screens/TRIURLSessionScreen.m
@@ -28,6 +28,8 @@ static NSString *HUGE_WIKIPEDIA_IMAGE = @"http://upload.wikimedia.org/wikipedia/
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.delayForSnapshot = 5.0;
+    
     self.total = 0;
     self.progressView.progress = 0.0;
     self.totalLabel.text = @"";
