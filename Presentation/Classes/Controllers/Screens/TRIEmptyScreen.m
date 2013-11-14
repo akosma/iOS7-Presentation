6aa76cebe215f7bb331f0998e362cd5158e67a1d
Adrian Kosmaczewski <ako@trifork.com>
2013-11-14 15:51:17 +0100

    Made the PDF generation faster by making the empty screen just not flash and continue

--- Presentation/Classes/Controllers/Screens/TRIEmptyScreen.m
+++ Presentation/Classes/Controllers/Screens/TRIEmptyScreen.m
@@ -14,25 +14,15 @@
 
 @implementation TRIEmptyScreen
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    self.delayForSnapshot = 0.3;
 }
 
-- (void)didReceiveMemoryWarning
+- (void)flashAndThen:(TRIBaseScreenControllerFlashCallback)callback
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    callback();
 }
 
 @end
