ea0faaa3f163433239467e3a985cade21dc9fa1a
Adrian Kosmaczewski <ako@trifork.com>
2013-11-14 08:15:05 +0100

    Added a new type of slide, for title slides

---
+++ Presentation/Classes/Controllers/Screens/TRITitleAndTextScreen.h
@@ -0,0 +1,13 @@
+//
+//  TRITitleAndTextScreen.h
+//  Presentation
+//
+//  Created by Adrian on 14/11/13.
+//  Copyright (c) 2013 Trifork GmbH. All rights reserved.
+//
+
+#import "TRIBaseScreenController.h"
+
+@interface TRITitleAndTextScreen : TRIBaseScreenController
+
+@end
---
+++ Presentation/Classes/Controllers/Screens/TRITitleAndTextScreen.m
@@ -0,0 +1,30 @@
+//
+//  TRITitleAndTextScreen.m
+//  Presentation
+//
+//  Created by Adrian on 14/11/13.
+//  Copyright (c) 2013 Trifork GmbH. All rights reserved.
+//
+
+#import "TRITitleAndTextScreen.h"
+
+@interface TRITitleAndTextScreen ()
+
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *textLabel;
+
+@end
+
+@implementation TRITitleAndTextScreen
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    NSString *title = self.definition[@"title"];
+    NSString *text = self.definition[@"contents"];
+    self.textLabel.text = text;
+    self.titleLabel.text = title;
+}
+
+@end
