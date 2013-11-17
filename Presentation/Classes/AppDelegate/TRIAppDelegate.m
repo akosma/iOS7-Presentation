e8cef1f45aed1a97debb2508e0e5f748d074c41c
Adrian Kosmaczewski <ako@trifork.com>
2013-11-17 18:09:12 +0100

    Prevent device from going to sleep

--- Presentation/Classes/AppDelegate/TRIAppDelegate.m
+++ Presentation/Classes/AppDelegate/TRIAppDelegate.m
@@ -21,6 +21,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    // This apps prevent the device from going to sleep
+    [UIApplication sharedApplication].idleTimerDisabled = YES;
+    
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.backgroundColor = [UIColor whiteColor];
     
